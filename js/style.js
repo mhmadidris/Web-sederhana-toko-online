57b068f55b92e7c07e62a36831adb95102cb4a28
mhmadidris <idris3097@gmail.com>
2021-07-20 00:19:13 +0700

    Update 0

---
+++ js/style.js
@@ -0,0 +1,6 @@
+function warna(pilih) {
+    if (pilih == 1) document.body.style.backgroundColor = 'red';
+    else if (pilih == 2) document.body.style.backgroundColor = 'yellow';
+    else if (pilih == 3) document.body.style.backgroundColor = 'green';
+    else if (pilih == 4) document.body.style.backgroundColor = 'white';
+}
